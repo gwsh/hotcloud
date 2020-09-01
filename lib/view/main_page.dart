7106005eaf94f94305521bf3e985b8b596a722e5
gwsh <shundong106@gmail.com>
2020-09-01 14:38:36 +0800

    feat(master) add branch master 2020-09-01

--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hotcloud/common/global.dart';
 import 'package:hotcloud/common/image_cache_widget.dart';
 import 'package:hotcloud/common/multi_state_widget.dart';
 import 'package:hotcloud/common/provider_widget.dart';
